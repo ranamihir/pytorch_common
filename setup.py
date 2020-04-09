44dfe6d8a3180ebedc3d308dadf93b61adbc9a29
Mihir Rana <ranamihir@gmail.com>
2020-04-09 02:00:59 -0400

    Added requirements for installing repo elsewhere.

--- setup.py
+++ setup.py
@@ -4,7 +4,11 @@ setup(
     # Application info
     name="pytorch_common",
 
-    description="DSP Pipeline",
+    description="Repo for common PyTorch code",
+
+    # Application author details:
+    author="Mihir Rana",
+    author_email="mrana@tripadvisor.com",
 
     # Version number:
     version="1.0",
@@ -17,6 +21,16 @@ setup(
 
     # packages that this package requires
     install_requires=[
+        "numpy==1.17.2",
+        "pandas==0.24.0",
+        "dask[dataframe]==2.3.0",
+        "toolz==0.10.0",
+        "scikit-learn==0.22.1",
+        "ipdb==0.13.2",
+        "pdbpp==0.10.2",
+        "dill==0.3.1.1",
+        "munch==2.5.0",
+        "locket==0.2.0",
     ],
 
     # Add config and sql files to the package
