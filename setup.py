a052cdde7dfd65bc9f5a7a063e10639f96fd5bea
Mihir Rana <ranamihir@gmail.com>
2020-04-29 05:20:53 -0400

    Added `transformers` as optional dependency.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author_email="ranamihir@gmail.com",
 
     # Version number:
-    version="1.0",
+    version="1.2",
 
     # Packages
     packages=find_packages(),
@@ -19,21 +19,24 @@ setup(
     # Tests (python -m unittest)
     test_suite="tests",
 
-    # packages that this package requires
+    # Packages that this package requires
     install_requires=[
-        "numpy==1.17.2",
-        "pandas==0.24.0",
+        "numpy>=1.17.2",
+        "pandas>=0.24.0",
         "dask[dataframe]==2.3.0",
         "toolz==0.10.0",
-        "scikit-learn==0.22.1",
-        "ipdb==0.13.2",
-        "pdbpp==0.10.2",
+        "scikit-learn>=0.22.1",
         "dill==0.3.1.1",
-        "munch==2.5.0",
+        "munch>=2.5.0",
         "locket==0.2.0",
     ],
 
+    # Optional dependencies
+    extras_require={
+        "nlp": ["transformers>=2.8.0"] # for NLP related projects
+    },
+
     # Add config and sql files to the package
     # https://python-packaging.readthedocs.io/en/latest/non-code-files.html
-    include_package_data=True,
+    include_package_data=True
 )
