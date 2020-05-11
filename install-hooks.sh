01d19003184eda10546d4e7b9ad846f05a96bf52
Mihir Rana <mrana@gitlab.dev.tripadvisor.com>
2020-05-12 04:25:21 +0530

    Fixed script.

--- install-hooks.sh
+++ install-hooks.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-GIT_DIR=$(git rev-parse --git-dir)
-
 echo "Installing hooks..."
-# this command creates symlink to our pre-push script
-ln -fs ../../pre-push.sh $GIT_DIR/hooks/pre-push
+cp pre-push.sh .git/hooks/pre-push
 echo "Done!"
