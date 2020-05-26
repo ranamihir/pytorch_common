1a7af9e3cc1738040da25939c3890e5500950417
Mihir Rana <ranamihir@gmail.com>
2020-05-26 16:50:59 -0400

    Added comments.

--- pre-push.sh
+++ pre-push.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+# The tests need to be run from within an environment that has pytorch installed (of course).
+# A `pytorch_common` conda env with the required dependencies may be installed by cloning this
+# repo and running:
+# `conda env create -f requirements-dev.yaml`
+# For running all the tests, you also need to have the `transformers` repo installed, otherwise
+# those will be skipped. This may be installed by running from within the `pytorch_common` env:
+# `pip install .[nlp]"`
+
 # Check for python breakpoints
 alias pythonfiles='find . -type f -name "*.py" | xargs git diff --cached --name-only $against'
 if [ -n "$(pythonfiles)" ]; then
@@ -22,6 +30,7 @@ source activate pytorch_common
 
 current_branch=`git branch | grep '*' | sed 's/* //'`
 
+### Uncomment the following code block to run tests only on master branch ###
 # if [ "$current_branch" = "master" ]; then
     # echo "You are about to push to master. Running all tests first..."
 #     python -m unittest discover tests
@@ -38,6 +47,7 @@ current_branch=`git branch | grep '*' | sed 's/* //'`
 # fi
 
 
+### Comment the following code block to prevent running tests on all branches ###
 echo "Running all tests..."
 python -m unittest discover tests
 if [ $? -eq 0 ]; then
